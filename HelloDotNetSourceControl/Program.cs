2b1e5309d06a9c6a21acf1597ba7600942b4da5f
CalvinSithideth <calvin.sithideth@gmail.com>
2019-07-08 07:43:15 -0700

    Refactored greeting message into separate method

--- HelloDotNetSourceControl/Program.cs
+++ HelloDotNetSourceControl/Program.cs
@@ -10,6 +10,12 @@ namespace HelloDotNetSourceControl
     {
         static void Main(string[] args)
         {
+            DisplayGreeting();
+            Console.ReadKey();
+        }
+
+        static void DisplayGreeting()
+        {
             Console.WriteLine("Hello World");
         }
     }
